17a753614f44c25b67a9c14249ba5e0731fb2690
Tboyphantom <ambatreasure0.gmail.com>
2026-02-09 04:28:54 +0100

    Add initial HTML, CSS, and JavaScript files for Vendor Web project

---
+++ Venget Web.js
@@ -0,0 +1,23 @@
+const h =document.getElementById("home")
+const a =document.getElementById("about")
+const l =document.getElementById("locate")
+const g =document.getElementById("gift")
+const i =document.getElementById("info")
+const s =document.getElementById("searchInput")
+h.addEventListener('click',() =>{
+if(true){
+i.innerHTML="Info Tab:"
+}
+})
+a.addEventListener('click',() =>{
+i.innerHTML="Venget is a fun name we came up with which means GET VENDORS"
+})
+l.addEventListener('click',() =>{
+i.innerHTML="Sorry we can relate to your curiousity,but we are remote right now"
+})
+g.addEventListener('click',() =>{
+i.innerHTML="We buy and sell gift cards and we will train you on it"
+})
+s.addEventListener('keyup',() =>{
+
+})
